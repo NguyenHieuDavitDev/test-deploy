f51d72ff1514aefd4c842e9e04b941568c407903
Hieu <huongdoan@MinhHieu.local>
2026-01-31 00:35:32 +0700

    Add vercel.json and api folder for deployment

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
+const adapter = new ExpressAdapter(server);
+
+let app: any;
+
+export default async function handler(req: any, res: any) {
+  if (!app) {
+    app = await NestFactory.create(AppModule, adapter);
+    app.enableCors();
+    await app.init();
+  }
+
+  server(req, res);
+}
