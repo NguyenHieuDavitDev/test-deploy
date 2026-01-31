61fdb786c43fe513a7be4a1f3b7a211115d14817
Hieu <huongdoan@MinhHieu.local>
2026-01-31 11:00:38 +0700

    Update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+
   app.enableCors();
-
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
-  app.use('/uploads', express.static('uploads'));
-
-  await app.listen(process.env.SERVER_PORT || 3000);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
